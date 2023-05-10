dd64f072507ae36ce945c0252846756d9c8231ae
Christoph Krieger <christoph.krieger@anaqor.io>
2023-05-10 09:33:58 +0200

    feat: add run command to execute a service

---
+++ src/commands/run/index.ts
@@ -0,0 +1,140 @@
+import {AuthenticatedCommand} from '../../model/command'
+import PlanqkService from '../../service/planqk-service';
+import ManagedServiceConfig from '../../model/managed-service-config';
+import ServiceConfigService from '../../service/service-config-service';
+import {Flags, ux} from '@oclif/core';
+import * as fs from 'fs-extra'
+import {ServiceDto} from '../../client/model/serviceDto';
+import {HttpError} from '../../client/api/apis';
+import * as path from 'path'
+import * as inquirer from 'inquirer';
+import waitUntil from 'async-wait-until';
+import {JobDto} from '../../client/model/jobDto';
+
+export default class Run extends AuthenticatedCommand {
+  static description = 'Creates a job execution of a PlanQK Service'
+
+  static examples = [
+    '$ planqk run',
+    '$ planqk run --detached',
+    '$ planqk run -d "{"values": [10,12]}" -p "{"round_up": true}"',
+    '$ planqk run --data-file=./input/dataV2.json --params-file=./input/paramsV2.json',
+  ]
+
+  planqkService!: PlanqkService
+
+  static flags = {
+    data: Flags.string({char: 'd', description: 'Input data as JSON string.', required: false}),
+    params: Flags.string({char: 'p', description: 'Input data as JSON string.', required: false}),
+    dataFile: Flags.string({aliases: ['data-file'], description: 'Relative path to file containing input data.', required: false}),
+    paramsFile: Flags.string({aliases: ['params-file'], description: 'Relative path to file containing params.', required: false}),
+    detached: Flags.boolean({description: 'Executes the job in detached mode, i.e., without waiting for it to finish.', required: false}),
+  }
+
+  async init(): Promise<void> {
+    await super.init()
+    this.planqkService = new PlanqkService(this, this.config, this.userConfig)
+  }
+
+  async run(): Promise<void> {
+    const {flags} = await this.parse(Run)
+
+    const serviceConfig: ManagedServiceConfig = ServiceConfigService.readServiceConfig()
+    if (!serviceConfig.serviceId) {
+      ux.error('No service id present in planqk.json. Either add the id of a deployed service or deploy your service by running `planqk up`.')
+    }
+
+    let service: ServiceDto
+    let serviceDefinitionId: string
+    try {
+      service = await this.planqkService.getService(serviceConfig.serviceId)
+      serviceDefinitionId = service.serviceDefinitions![0].id!
+    } catch {
+      ux.error('Service referenced by property serviceId in planqk.json does not exist.')
+    }
+
+    const data = flags.data ? this.fixJsonString(flags.data) : await this.getInputData(flags.dataFile || '/input/data.json')
+    const params = flags.params ? this.fixJsonString(flags.params) : await this.getParams(flags.paramsFile || '/input/params.json')
+
+    const payload = {
+      inputData: data,
+      parameters: params,
+      persistResult: false,
+      serviceDefinitionId: serviceDefinitionId,
+    }
+
+    let job: JobDto
+    job = await this.planqkService.runJob(payload)
+
+    if (flags.detached) {
+      ux.info(`Job (${job.id}) created.`)
+      this.exit()
+    }
+
+    ux.action.start(`Running Job (${job.id})`)
+
+    try {
+      await waitUntil(async () => {
+        job = await this.planqkService.getJobById(job.id as string)
+        return job.status === JobDto.StatusEnum.Success || job.status === JobDto.StatusEnum.Failed
+      },
+      {
+        timeout: 10 * 60 * 1000, // 10 minute timeout
+        intervalBetweenAttempts: 5000, // every 5 seconds
+      })
+    } catch {
+      // ignore wait until next attempt
+    }
+
+    const jobDetailsLink = `https://platform.planqk.de/jobs/${job.id}`
+    if (job.status === JobDto.StatusEnum.Success) {
+      ux.action.stop('Job succeeded.')
+      ux.info(`See result at \u001B]8;;${jobDetailsLink}\u0007${jobDetailsLink}\u001B]8;;\u0007\``)
+    } else if (job.status === JobDto.StatusEnum.Failed) {
+      ux.action.stop('Job failed.')
+      ux.info(`See details at \u001B]8;;${jobDetailsLink}\u0007${jobDetailsLink}\u001B]8;;\u0007\``)
+    } else {
+      ux.action.stop('Timeout polling status.')
+      ux.info(`See status details at \u001B]8;;${jobDetailsLink}\u0007${jobDetailsLink}\u001B]8;;\u0007\``)
+    }
+
+    this.exit()
+  }
+
+  async getInputData(relativePath: string): Promise<string | undefined> {
+    const location = path.join(process.cwd(), relativePath)
+
+    if (!fs.existsSync(location)) {
+      const response = await inquirer.prompt({
+        name: 'confirm',
+        message: `No input file found under ${relativePath}. Run without input data?`,
+        type: 'confirm',
+      })
+      if (response.confirm) return
+      this.exit()
+    }
+
+    return JSON.stringify(fs.readJsonSync(location, {encoding: 'utf-8'}))
+  }
+
+  async getParams(relativePath: string): Promise<string | undefined> {
+    const location = path.join(process.cwd(), relativePath)
+
+    if (!fs.existsSync(location)) {
+      const response = await inquirer.prompt({
+        name: 'confirm',
+        message: `No params file found under ${relativePath}. Run without params?`,
+        type: 'confirm',
+      })
+      if (response.confirm) return
+      this.exit()
+    }
+
+    return JSON.stringify(fs.readJsonSync(location, {encoding: 'utf-8'}))
+  }
+
+  fixJsonString(jsonString: string): string {
+    const regex = /([,{])(\s*)(\w+?)\s*:/g;
+    return jsonString.replace(regex, '$1"$3":');
+  }
+}
--- src/service/planqk-service.ts
+++ src/service/planqk-service.ts
@@ -8,18 +8,30 @@ import {BuildJobDto, ServiceDefinitionDto} from '../client/model/models'
 import {HttpError} from '../client/api/apis'
 import {ReadStream} from 'fs-extra'
 import ManagedServiceConfig from '../model/managed-service-config'
+import axios from 'axios';
+import {ServicePlatformJobsApi, ServicePlatformJobsApiApiKeys} from '../client/api/servicePlatformJobsApi';
+import {CreateJobRequest} from '../client/model/createJobRequest';
+import {JobDto} from '../client/model/jobDto';
 
 export default class PlanqkService extends CommandService {
   serviceApi: ServicePlatformServicesApi
+  jobApi: ServicePlatformJobsApi
 
   constructor(cmd: Command, config: Config, userConfig: UserConfig) {
     super(cmd, config, userConfig)
     this.serviceApi = new ServicePlatformServicesApi()
     this.serviceApi.setApiKey(ServicePlatformServicesApiApiKeys.apiKey, userConfig.auth?.value as string)
+    this.jobApi = new ServicePlatformJobsApi()
+    this.jobApi.setApiKey(ServicePlatformJobsApiApiKeys.apiKey, userConfig.auth?.value as string)
     if (userConfig.endpoint) {
       this.serviceApi.basePath = userConfig.endpoint.basePath || defaultBasePath
       this.serviceApi.defaultHeaders = userConfig.endpoint.defaultHeaders || {}
     }
+  }
+
+  async getService(id: string): Promise<ServiceDto> {
+    const response = await this.serviceApi.getService(id)
+    return response.body
   }
 
   async createService(serviceConfig: ManagedServiceConfig, userCode: ReadStream): Promise<ServiceDto> {
@@ -76,5 +88,23 @@ export default class PlanqkService extends CommandService {
   async getBuildJobStatus(service: ServiceDto, serviceDefinition: ServiceDefinitionDto): Promise<BuildJobDto.StatusEnum | undefined> {
     const buildJob = await this.getBuildJob(service!, serviceDefinition!)
     return buildJob.status
+  }
+
+  async runJob(payload: CreateJobRequest): Promise<JobDto> {
+    try {
+      const response =  await this.jobApi.createJob(payload, undefined, {headers: {'X-Auth-Token': this.userConfig.auth!.value}})
+      return response.body
+    } catch (error) {
+      if (error instanceof HttpError) {
+        this.cmd.error(`Error creating job: ${error.response.statusCode} - ${error.response.statusMessage}`)
+      }
+
+      throw new Error('Internal error occurred, please contact your PlanQK administrator')
+    }
+  }
+
+  async getJobById(id: string): Promise<JobDto> {
+    const response = await this.jobApi.getJobById(id, undefined, {headers: {'X-Auth-Token': this.userConfig.auth!.value}})
+    return response.body;
   }
 }
