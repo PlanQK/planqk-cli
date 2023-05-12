64045a0d18b05f9ecd18380a8fe148f965f9fe75
Christoph Krieger <christoph.krieger@anaqor.io>
2023-05-12 10:26:55 +0200

    chore: config command examples

--- src/commands/config/index.ts
+++ src/commands/config/index.ts
@@ -4,7 +4,8 @@ export default class Config extends AbstractCommand {
   static description = 'Manage planqk CLI context configuration.'
 
   static examples = [
-    '$ planqk config get-context',
+    '$ planqk config:get-context',
+    '$ planqk config:set-context',
   ]
 
   async run(): Promise<void> {
