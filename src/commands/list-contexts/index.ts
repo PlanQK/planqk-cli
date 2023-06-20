6ff83ba087c889b1b4f917e5f4dc7f43d849fd99
Michael Wurster <miwurster@googlemail.com>
2023-06-20 14:35:43 +0200

    feat: add list-contexts command

---
+++ src/commands/list-contexts/index.ts
@@ -0,0 +1,49 @@
+import {AuthenticatedCommand} from '../../model/command'
+import Account from '../../model/account'
+import {ux} from '@oclif/core'
+import PlanqkService from '../../service/planqk-service'
+
+interface Context extends Record<string, unknown> {
+  id: string
+  displayName: string
+  isOrganization: boolean
+}
+
+export default class ListContexts extends AuthenticatedCommand {
+  planqkService!: PlanqkService
+
+  static description = 'Retrieves the available contexts, i.e., the personal or organizational accounts you are currently working with.'
+
+  static examples = [
+    '$ planqk list-contexts',
+  ]
+
+  async init(): Promise<void> {
+    await super.init()
+    this.planqkService = new PlanqkService(this, this.config, this.userConfig)
+  }
+
+  async run(): Promise<void> {
+    ux.action.start('Loading available contexts')
+    const accounts: Account[] = await this.planqkService.getAccounts()
+    ux.action.stop('Done')
+
+    const c: Context[] = accounts.map(account => {
+      return {id: account.id, displayName: account.displayName, isOrganization: account.isOrganization}
+    })
+
+    ux.table(c, {
+      displayName: {
+        header: 'Name',
+        minWidth: 15,
+      },
+      id: {
+        header: 'ID',
+      },
+      isOrganization: {
+        header: 'Organization',
+        get: row => row.isOrganization ? ' \u{2713}' : ' \u{2717}',
+      },
+    })
+  }
+}
