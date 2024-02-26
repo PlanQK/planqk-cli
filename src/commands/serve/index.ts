f16e3045b1edbf666206be356176b9e630128d92
radufalcusan <radu.falcusan@anaqor.io>
2024-02-26 17:34:29 +0100

    fix: repair lint errors

--- src/commands/serve/index.ts
+++ src/commands/serve/index.ts
@@ -3,10 +3,11 @@ import {execSync} from 'child_process'
 import {Flags} from '@oclif/core'
 
 export default class Serve extends AbstractCommand {
-
   static description = 'Allows the creation, status check and deletion of service instances.'
 
-  static examples = ['$ planqk serve -p <port>',]
+  static examples = [
+    '$ planqk serve -p <port>',
+  ]
 
   static flags = {
     port: Flags.integer({char: 'p', description: 'The port on which the serve command is executed', required: false}),
