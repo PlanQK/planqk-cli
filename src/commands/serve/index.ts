09a9bc2f70e0b93b8446d3fa1640a22c9dda532e
radufalcusan <radu.falcusan@anaqor.io>
2024-02-26 17:28:33 +0100

    feat: add planqk serve command

---
+++ src/commands/serve/index.ts
@@ -0,0 +1,25 @@
+import {AbstractCommand} from '../../model/command'
+import {execSync} from 'child_process'
+import {Flags} from '@oclif/core'
+
+export default class Serve extends AbstractCommand {
+
+  static description = 'Allows the creation, status check and deletion of service instances.'
+
+  static examples = ['$ planqk serve -p <port>',]
+
+  static flags = {
+    port: Flags.integer({char: 'p', description: 'The port on which the serve command is executed', required: false}),
+  }
+
+  async run(): Promise<void> {
+    try {
+      const {flags} = await this.parse(Serve)
+      const hostPort = flags.port ? flags.port : 8081;
+
+      execSync(`docker run -p ${hostPort}:8001 -v "$(pwd):/user_code" ghcr.io/planqk/planqk-cli-serve:master`, {stdio: 'inherit'});
+    } catch (error: any) {
+      this.error(`Error: ${error.message}`);
+    }
+  }
+}
