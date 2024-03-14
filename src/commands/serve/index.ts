51398a38e04b0e3cd4f67e8fdec19ddd209d6bd3
radufalcusan <radu.falcusan@anaqor.io>
2024-03-14 11:00:02 +0100

    feat: ensure latest image before starting container and add a more user-friendly UI for serve command

--- src/commands/serve/index.ts
+++ src/commands/serve/index.ts
@@ -1,6 +1,6 @@
 import {AbstractCommand} from '../../model/command'
-import {execSync} from 'child_process'
-import {Flags} from '@oclif/core'
+import {execSync, StdioOptions} from 'child_process'
+import {Flags, ux} from '@oclif/core'
 
 export default class Serve extends AbstractCommand {
   static description = 'Allows the creation, status check and deletion of service instances.'
@@ -13,14 +13,31 @@ export default class Serve extends AbstractCommand {
     port: Flags.integer({char: 'p', description: 'The port on which the serve command is executed', required: false}),
   }
 
-  async run(): Promise<void> {
+  executeCommand(command: string, stdioOption?: StdioOptions): void {
     try {
-      const {flags} = await this.parse(Serve)
-      const hostPort = flags.port ? flags.port : 8081;
-
-      execSync(`docker run -p ${hostPort}:8001 -v "$(pwd):/user_code" ghcr.io/planqk/planqk-cli-serve:master`, {stdio: 'inherit'});
+      execSync(`${command}`, {stdio: stdioOption ? stdioOption : 'inherit'});
     } catch (error: any) {
       this.error(`Error: ${error.message}`);
     }
+  }
+
+  removeAndPullImage(): void {
+    ux.action.start('Ensuring latest image');
+    this.executeCommand('docker rmi -f ghcr.io/planqk/planqk-cli-serve:master && docker pull ghcr.io/planqk/planqk-cli-serve:master', 'ignore');
+    ux.action.stop()
+  }
+
+  startContainer(hostPort: string): void {
+    ux.action.start('Start Container');
+    this.executeCommand(`docker run -p ${hostPort}:8001 -v "$(pwd):/user_code" ghcr.io/planqk/planqk-cli-serve:master`);
+    ux.action.stop('Stop Container');
+  }
+
+  async run(): Promise<void> {
+    const {flags} = await this.parse(Serve);
+    const hostPort = flags.port ? flags.port : 8081;
+
+    this.removeAndPullImage();
+    this.startContainer(hostPort.toString());
   }
 }
