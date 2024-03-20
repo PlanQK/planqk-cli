32b46ce96144dde0cc9b5b5b0d19dbd5ea7d953b
radufalcusan <radu.falcusan@anaqor.io>
2024-03-20 14:38:05 +0100

    chore: separate build container from start container steps for planqk serve command

--- src/commands/serve/index.ts
+++ src/commands/serve/index.ts
@@ -1,6 +1,8 @@
 import {AbstractCommand} from '../../model/command'
 import {execSync, StdioOptions} from 'child_process'
-import {Flags, ux} from '@oclif/core'
+import {Flags} from '@oclif/core'
+import Listr from 'listr';
+import {exec} from 'node:child_process';
 
 export default class Serve extends AbstractCommand {
   static description = 'Allows the creation, status check and deletion of service instances.'
@@ -21,23 +23,45 @@ export default class Serve extends AbstractCommand {
     }
   }
 
-  removeAndPullImage(): void {
-    ux.action.start('Ensuring latest image');
-    this.executeCommand('docker rmi -f ghcr.io/planqk/planqk-cli-serve:master && docker pull ghcr.io/planqk/planqk-cli-serve:master', 'ignore');
-    ux.action.stop()
-  }
-
-  startContainer(hostPort: string): void {
-    ux.action.start('Start Container');
-    this.executeCommand(`docker run -p ${hostPort}:8001 -v "$(pwd):/user_code" ghcr.io/planqk/planqk-cli-serve:master`);
-    ux.action.stop('Stop Container');
+  async executeAsyncCommand(command: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      exec(command, (error, stdout, stderr) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve({stdout, stderr});
+        }
+      });
+    });
   }
 
   async run(): Promise<void> {
     const {flags} = await this.parse(Serve);
     const hostPort = flags.port ? flags.port : 8081;
 
-    this.removeAndPullImage();
-    this.startContainer(hostPort.toString());
+    const removeAndPullImageCommand = 'docker rmi -f serve && docker pull serve';
+
+    const containerName = 'planqk-cli-serve';
+    const buildCommand = `docker run -p ${hostPort}:8001 -v "$(pwd):/user_code" --name ${containerName} serve`;
+    const runCommand = `docker start -a ${containerName}`;
+
+    const tasks = new Listr([
+      {
+        title: 'Ensuring latest image',
+        task: () => Promise.resolve(this.executeAsyncCommand(removeAndPullImageCommand)),
+      },
+      {
+        title: 'Building container',
+        task: () => Promise.resolve(this.executeAsyncCommand(buildCommand)),
+      },
+      {
+        title: 'Starting container',
+        task: () => this.executeCommand(runCommand),
+      },
+    ]);
+
+    tasks.run().catch(error => {
+      console.error(error);
+    });
   }
 }
