c2c6be64b818284090cfdc8c3a76040a4ddb3e93
radufalcusan <radu.falcusan@anaqor.io>
2024-04-03 12:56:08 +0200

    fix: simplify serve command by creating a new container with every execution

--- src/commands/serve/index.ts
+++ src/commands/serve/index.ts
@@ -15,8 +15,8 @@ export default class Serve extends AbstractCommand {
     port: Flags.integer({char: 'p', description: 'The port on which the serve command is executed', required: false}),
   }
 
-  static image = 'ghcr.io/planqk/planqk-cli-serve:master'
-  static containerName = 'planqk-cli-serve'
+  static IMAGE = 'ghcr.io/planqk/planqk-cli-serve:master'
+  static CONTAINER_NAME = 'planqk-cli-serve'
 
   executeCommand(command: string, stdioOption?: StdioOptions): void {
     try {
@@ -38,70 +38,32 @@ export default class Serve extends AbstractCommand {
     });
   }
 
-  async buildContainer(containerName: string, hostPort: string): Promise<any> {
-    const wasContainerCreatedCommand = `if docker inspect "${containerName}" >/dev/null 2>&1; then
-    echo "true"
-else
-    echo "false"
-fi`;
-
-    const wasContainerCreatedResponse = await this.executeAsyncCommand(wasContainerCreatedCommand)
-
-    if (wasContainerCreatedResponse.stdout.toString().includes('true')) {
-      // Create a new image from the old one to be able to mount the new volume if the directory has changed
-      const imageName = containerName + this.generateRandomString(30);
-      const reBuildCommand = `docker commit ${containerName} ${imageName} && docker rm ${containerName} && ${this.getBuildCommand(hostPort, containerName, imageName)}`
-      await this.executeAsyncCommand(reBuildCommand);
-    } else {
-      await this.executeAsyncCommand(this.getBuildCommand(hostPort, containerName, Serve.image));
-    }
-  }
-
   async run(): Promise<void> {
     const {flags} = await this.parse(Serve);
     const hostPort = flags.port ? flags.port : 8081;
 
-    const removeAndPullImageCommand = `docker rmi -f ${Serve.image} && docker pull ${Serve.image}`;
-
-    const runCommand = `docker start -a ${Serve.containerName}`;
-
     const tasks = new Listr([
       {
         title: 'Ensuring latest image',
-        task: () => this.executeAsyncCommand(removeAndPullImageCommand).catch(error => {
+        task: () => this.executeAsyncCommand(`docker rmi -f ${Serve.IMAGE} && docker pull ${Serve.IMAGE}`).catch(error => {
           this.error(`${error.message}`);
         }),
       },
       {
         title: 'Building container',
-        task: () => this.buildContainer(Serve.containerName, hostPort.toString())
+        task: () => this.executeAsyncCommand(`docker run -p ${hostPort}:8001 -v "$(pwd):/user_code" --name ${Serve.CONTAINER_NAME} ${Serve.IMAGE}`)
           .catch(error => {
             this.error(`${error.message}`);
           }),
       },
       {
         title: 'Starting container',
-        task: () => Promise.resolve(this.executeCommand(runCommand)),
+        task: () => Promise.resolve(this.executeCommand(`docker start -a ${Serve.CONTAINER_NAME}`)),
       },
     ]);
 
     tasks.run().catch(error => {
       console.error(error);
-    });
-  }
-
-  generateRandomString(length: number): string {
-    const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-    let result = '';
-    for (let i = 0; i < length; i++) {
-      const randomIndex = Math.floor(Math.random() * characters.length);
-      result += characters.charAt(randomIndex);
-    }
-
-    return result;
-  }
-
-  getBuildCommand(hostPort: string, containerName: string, imageName: string): string {
-    return `docker run -p ${hostPort}:8001 -v "$(pwd):/user_code" --name ${containerName} ${imageName}`
+    }).finally(() => this.executeAsyncCommand(`docker rm ${Serve.CONTAINER_NAME}`))
   }
 }
